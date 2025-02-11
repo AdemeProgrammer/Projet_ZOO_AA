47909158ff3150e0ce109f5e0b60980e4752a553
Berynd <gabriel.berinde93@icloud.com>
2025-02-11 09:38:53 +0100

    feat: soigneur

--- src/Zoo/Soigneur.java
+++ src/Zoo/Soigneur.java
@@ -1,4 +1,17 @@
 package Zoo;
 
 public class Soigneur {
+    private String nom;
+    private String prenom;
+    private String specialite;
+    private boolean diagnostique;
+
+    public Soigneur(String nom, String prenom, String specialite, boolean diagnostique) {
+        this.nom = specialite;
+        this.prenom = prenom;
+        this.specialite = specialite;
+
+
+    }
 }
+
